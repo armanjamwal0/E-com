d6232d58b8b2c2ce255866cbb6547bb696a66d19
Armanjamwal <armanjamwal@hotmail.com>
2025-06-09 18:00:48 +0530

    add some info in axios

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "http://127.0.0.1:5000",// here axios use for #_______________________________________
+  //An Axios instance is a customized version of Axios with default settings 
+  // (like base URL, headers, tokens, etc.) pre-configured. Instead of repeating those settings every time, 
+  // you configure them once.
   withCredentials: true,             // allows cookies if you ever switch to them
 });
 
