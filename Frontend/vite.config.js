d7d221c2027cbcda165d1969ad4d7e32f9308a38
Armanjamwal <armanjamwal@hotmail.com>
2025-06-21 01:14:05 +0530

    init commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,9 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
   server: {
-    allowedHosts: ['b5dd-2402-8100-3953-6a9c-8c3f-43a8-f33a-7dc7.ngrok-free.app'], // ⬅️ your ngrok domain here
+       allowedHosts: [
+      'homework-quantitative-navigate-wesley.trycloudflare.com'  // ⬅️ Add your Cloudflare tunnel host here
+    ], // ⬅️ your ngrok domain here
     host: true, // allows Vite to listen on all interfaces
   },
 })
