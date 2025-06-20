347b39a5da3e67ae10365e695e6f0f80327850a2
Armanjamwal <armanjamwal@hotmail.com>
2025-06-21 01:53:58 +0530

    init commit

--- Frontend/src/component/Pages/home.jsx
+++ Frontend/src/component/Pages/home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import api from "../../api";
 import { useNavigate } from "react-router-dom";
-import LogoutButton from "./logout";
+
 
 
 function Home({ setAuthenticated }) {
