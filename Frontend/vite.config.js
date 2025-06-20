ad8cf34d1f09941e3fb1df38a7f25038c391c2a9
Armanjamwal <armanjamwal@hotmail.com>
2025-06-21 01:53:58 +0530

    init commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
   server: {
        allowedHosts: [
-      'homework-quantitative-navigate-wesley.trycloudflare.com'  // ⬅️ Add your Cloudflare tunnel host here
+      'ridge-frost-subscribers-undergraduate.trycloudflare.com'  // ⬅️ Add your Cloudflare tunnel host here
     ], // ⬅️ your ngrok domain here
     host: true, // allows Vite to listen on all interfaces
   },
